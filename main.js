3a2747d3c160e18dd3ef1c9fa9ad27ab157b0c82
Ethan36Z <zhangethan36@gmail.com>
2025-10-06 01:51:34 -0700

    Update main.js

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ document.getElementById("taskForm").addEventListener("submit", function(event){
     const img = document.createElement("img");
 
     // 创建删除图片
-    img.src = "./delete.png";
+    img.src = "delete.png";
     img.alt = "Delete";
     img.style.width = "16px";
     img.style.height = "16px";
@@ -42,3 +42,4 @@ document.getElementById("taskForm").addEventListener("submit", function(event){
     document.getElementById("taskInput").value= "";
 
 })
+
